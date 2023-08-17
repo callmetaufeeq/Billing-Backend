e1ede7d417422e62b750b0f8dba3d8f401ccadea
Taufeeq <123618277+callmetaufeeq@users.noreply.github.com>
2023-08-17 15:29:17 +0530

    Delete Billing directory

--- Billing/src/main/java/com/tw/repository/ItemReository.java
+++
@@ -1,9 +0,0 @@
-package com.tw.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tw.model.Item;
-
-public interface ItemReository extends JpaRepository<Item, Long>{
-
-}
--- Billing/src/main/java/com/tw/service/CountService.java
+++
@@ -1,9 +0,0 @@
-package com.tw.service;
-
-import com.tw.model.Count;
-
-public interface CountService {
-	
-	String countInvoice();
-
-}
