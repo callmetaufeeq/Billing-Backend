f8a9f0216aca8bc8ccc351e1c11bfd69802b6cbe
Taufeeq <123618277+callmetaufeeq@users.noreply.github.com>
2023-08-18 16:06:34 +0530

    count change

--- src/main/java/com/tw/serviceImpl/CountServiceImpl.java
+++ src/main/java/com/tw/serviceImpl/CountServiceImpl.java
@@ -19,8 +19,7 @@ public class CountServiceImpl implements CountService {
 		Count obj = countRepo.getOne(1L);
 		int i=obj.getCount();
 		String s ="INV-"+i;
-		obj.setCount(i+1);
-		countRepo.save(obj);
+		
 		return s;
 	}
 
--- src/main/java/com/tw/serviceImpl/CustomerServiceImpl.java
+++ src/main/java/com/tw/serviceImpl/CustomerServiceImpl.java
@@ -8,7 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tw.model.Count;
 import com.tw.model.Customer;
+import com.tw.repository.CountRepository;
 import com.tw.repository.CustomerRepository;
 import com.tw.service.CustomerService;
 
@@ -16,15 +18,23 @@ import jakarta.transaction.Transactional;
 
 @Service
 @Transactional
-public class CustomerServiceImpl implements CustomerService{
+public class CustomerServiceImpl implements CustomerService {
 	private Logger logger = LoggerFactory.getLogger(getClass());
-	
+
 	@Autowired
 	CustomerRepository customerRepo;
+	
+	@Autowired
+	CountRepository countRepo;
 
 	@Override
 	public String saveData(Customer customer) {
-		customerRepo.save(customer);
+		Customer c=customerRepo.save(customer);
+		if(c!=null) {
+			Count obj = countRepo.getOne(1L);
+			obj.setCount(obj.getCount()+1);
+			countRepo.save(obj);
+		}
 		return "Save Success ";
 	}
 
@@ -33,7 +43,6 @@ public class CustomerServiceImpl implements CustomerService{
 		return customerRepo.findAll();
 	}
 
-	
 	@Override
 	public Customer getCustomerById(Long id) {
 		Optional<Customer> cus = customerRepo.findById(id);
@@ -45,24 +54,5 @@ public class CustomerServiceImpl implements CustomerService{
 		customerRepo.deleteById(id);
 		return "Deleted Successfully";
 	}
-	
-
-//	@Override
-//	public String saveData(CustomerDto dto) {
-//		logger.info("Creating user: " + dto);
-//		Customer c = new Customer();
-//		if (dto.getId() != null) {
-//			c = customerRepo.getOne(dto.getId());
-//		}
-//		c.setId(dto.getId());
-//		c.setCustomerName(dto.getCustomerName());
-//		c.setAddress(dto.getAddress());
-//		c.setMobileNumber(dto.getMobileNumber());
-//		customerRepo.save(c);
-//		return ("ok");;
-//	}
-    
-	
-	
 
 }
